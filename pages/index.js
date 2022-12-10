a129c1122707b4872436fe5883c049826b262d09
danieldevgki <daniels@gkigroup.com>
2022-12-10 14:25:30 +0200

    update web3.js, factory.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
+
+// factory contract
+import Factory from '../ethereum/factory';
+
 import Meta from '../components/Meta';
 import PageHeader from '../components/PageHeader';
 
 export default function Home() {
+  const [campaigns, setCampaigns] = useState([]);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const campaigns = await Factory.methods.getDeployedCampaigns().call();
+  //       console.log(campaigns);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   })();
+  // });
+
   return (
     <>
       <Meta />
@@ -9,3 +27,14 @@ export default function Home() {
     </>
   );
 }
+
+// export async function getServerSideProps() {
+//   // Fetch data from external API
+//   const res = await fetch(`http://localhost:3000/api/factory`);
+
+//   console.log(res);
+//   const data = JSON.parse(JSON.stringify(res));
+
+//   // Pass data to the page via props
+//   return { props: { data } };
+// }
