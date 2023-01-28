b5d9c2e5775182191a178aef88975530d52b6993
danieldevgki <daniels@gkigroup.com>
2023-01-28 13:34:23 +0200

    update campaign.sol

--- pages/campaigns/details.js
+++ pages/campaigns/details.js
@@ -1,3 +1,15 @@
-export default function CampaignDetails(campaign) {
-  return <p>lala</p>;
+import Meta from "../../components/Meta";
+import PageHeader from "../../components/PageHeader";
+
+export default function Details(campaign) {
+  return (
+    <>
+      <Meta title="Kick Coin | Campaign Details" />
+
+      <PageHeader
+        title="Campaign"
+        description="Initialize a Campaign for your purpose"
+      />
+    </>
+  );
 }
