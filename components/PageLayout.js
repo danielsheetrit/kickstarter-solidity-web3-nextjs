b263acedf94b69c8dde547abe965980ea88cbbb7
danieldevgki <daniels@gkigroup.com>
2023-02-20 20:24:43 +0200

    change loader

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,41 +1,17 @@
-import { useState, useEffect } from 'react';
-// next
-import { useRouter } from 'next/router';
 import { Container } from 'semantic-ui-react';
 
 // components
 import Nav from './Nav';
-import Loader from './Loader';
 
 export default function PageLayout({ children }) {
-  const [loading, setLoading] = useState(false);
-
-  const router = useRouter();
-
-  // loader logic
-  useEffect(() => {
-    const handleStart = (url) => url !== router.asPath && setLoading(true);
-    const handleComplete = (url) => url === router.asPath && setLoading(false);
-    const handleError = () => setLoading(false);
-
-    router.events.on('routeChangeStart', handleStart);
-    router.events.on('routeChangeComplete', handleComplete);
-    router.events.on('routeChangeError', handleError);
-
-
-    return () => {
-      router.events.off('routeChangeStart', handleStart);
-      router.events.off('routeChangeComplete', handleComplete);
-    };
-  }, [router]);
-
   return (
-    <Container style={{
-      paddingBottom: 100
-    }}>
+    <Container
+      style={{
+        paddingBottom: 100,
+      }}
+    >
       <Nav />
-      {loading && <Loader />}
-      {!loading && children}
+      {children}
     </Container>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,15 @@ import 'semantic-ui-css/semantic.min.css';
 // components
 import PageLayout from '../components/PageLayout';
 
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+//Binding events.
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
   return (
     <PageLayout>
