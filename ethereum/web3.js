ddef8d46c87aae0efe220590d0b0d282ef7723ef
danieldevgki <daniels@gkigroup.com>
2023-02-20 12:49:42 +0200

    apply changes 2

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -14,7 +14,7 @@ if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     'https://goerli.infura.io/v3/e9a4ce1ec2b74c6cabbdef776c848ad1'
   );
   web3 = new Web3(provider);
-  provider.engine.stop();
+  // provider.engine.stop();
 }
 
 
